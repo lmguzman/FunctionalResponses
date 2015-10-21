3b56636d88d71eb5b9cb1ff74ce059dd48f9bd60
Melissa <guzman@zoology.ubc.ca>
2015-10-21 10:10:56 -0700

    Added the all models function

--- R/ExternalFunctions.R
+++ R/ExternalFunctions.R
@@ -1,9 +1,11 @@
 
+source('InternalFunctions.R')
+detach()
 
-OneTreatmentModel <- function(offered, eaten){
+ OneTreatmentModel <- function(offered, eaten){
 
   #packages required
-  require(bbmle)
+  library(bbmle)
 
   #Making sure data has at least 2 columns
   #if(nrow(offered) != nrow(eaten)) stop ("Prey offered and eaten dont have the same lenght")
@@ -14,8 +16,8 @@ OneTreatmentModel <- function(offered, eaten){
 
   # we set up some plausible starting values with upper and lower limits
   start.vals = list(b = 0.004, q = 1.6, h = 0.5)
-  lower.lims = c(b = 1e-10, q = -2, h = 1e-10)
-  upper.lims = c(b = 1, q = 3, h = 3)
+  lower.lims = c(b = 1e-10, q = -3, h = 1e-10)
+  upper.lims = c(b = 3, q = 3, h = 3)
 
   #Using maximum likelyhood and NLL.oneT
   full.mod <- mle2(NLL.oneT, start=start.vals, data = dd.ml,
@@ -26,11 +28,177 @@ OneTreatmentModel <- function(offered, eaten){
 }
 
 
+ ## Treament has to be a vector of y and n
 
-simple1<- dd$offered
-simple2<-dd$ne.int.0
+TwoTreatmentModel<- function(offered, eaten, treatment){
+  #packages required
+  library(bbmle)
+
+  #Making sure data has at least 2 columns
+  #if(nrow(offered) != nrow(eaten)) stop ("Prey offered and eaten dont have the same lenght")
+
+  #We are organizing the data
+  dd.ml = list(T = 4, P = 1, Initial=offered,
+               Killed=eaten, ind=ifelse(treatment=="no", 0, 1))
 
 
-OneTreatmentModel(simple1,simple2)
+
+  ##### FULL MODEL ####
+
+  #Start values and limits
+  start.vals = list(b.int=0.14, b.ind=0.004,
+                    q.int=0.01, q.ind=1.6,
+                    h.int=0.357, h.ind=1.68)
+  lower.lims=c(b.int=1e-10, b.ind=1e-10,
+               q.int=-2, q.ind=-2,
+               h.int=1e-10, h.ind=1e-10)
+  upper.lims=c(b.int=1, b.ind=1,
+               q.int=3, q.ind=3,
+               h.int=3, h.ind=3)
+
+  full.mod <- mle2(NLL.real.full, start=start.vals, data=dd.ml,
+                   method="L-BFGS-B",
+                   lower=lower.lims,
+                   upper=upper.lims)
+  Full.Mod<-list('Coefficients' = attributes(summary(full.mod))$coef, 'AIC' = AIC(full.mod))
+
+print('full mode done!')
+
+  ### B Constant ###
+  start.vals = list(b=0.14,
+                    q.int=0.01, q.ind=1.6,
+                    h.int=0.357, h.ind=1.68)
+  lower.lims=c(b=1e-10,
+               q.int=-2, q.ind=-2,
+               h.int=1e-10, h.ind=1e-10)
+  upper.lims=c(b=1,
+               q.int=3, q.ind=3,
+               h.int=3, h.ind=3)
+  b.fixed.mod <- mle2(NLL.real.b, start=start.vals, data=dd.ml,
+                      method="L-BFGS-B",
+                      lower=lower.lims,
+                      upper=upper.lims)
+  B.Fixed.Mod<-list('Coefficients' = attributes(summary(b.fixed.mod))$coef, 'AIC' = AIC(b.fixed.mod))
+
+print('B mode done!')
+
+  ### H Constant ###
+  start.vals = list(b.int=0.1, b.ind=0.04,
+                  q.int=0.01, q.ind=1.6,
+                  h=1.3)
+  lower.lims=c(b.int=1e-10, b.ind=1e-10,
+               q.int=-2, q.ind=-2,
+               h=1e-10)
+  upper.lims=c(b.int=1, b.ind=1,
+               q.int=3, q.ind=3,
+               h=3)
+  h.fixed.mod <- mle2(NLL.real.h, start=start.vals, data=dd.ml,
+                      method="L-BFGS-B",
+                      lower=lower.lims,
+                      upper=upper.lims)
+  H.Fixed.Mod<-list('Coefficients' = attributes(summary(h.fixed.mod))$coef, 'AIC' = AIC(h.fixed.mod))
+
+  print('H mode done!')
+
+  #### Q Constant ###
+  start.vals = list(b.int=0.14, b.ind=0.004,
+                    q=1.6,
+                    h.int=0.357, h.ind=1.68)
+  lower.lims=c(b.int=1e-10, b.ind=1e-10,
+               q=-2,
+               h.int=1e-10, h.ind=1e-10)
+  upper.lims=c(b.int=1, b.ind=1,
+               q=3,
+               h.int=3, h.ind=3)
+  q.fixed.mod <- mle2(NLL.real.q, start=start.vals, data=dd.ml,
+                      method="L-BFGS-B",
+                      lower=lower.lims,
+                      upper=upper.lims)
+  Q.Fixed.Mod<-list('Coefficients' = attributes(summary(q.fixed.mod))$coef, 'AIC' = AIC(q.fixed.mod))
+
+  print('Q mode done!')
+
+  ### B and H constant ###
+  start.vals = list(b=0.1,
+                    q.int=0.01, q.ind=0.6,
+                    h=0.35)
+  lower.lims=c(b=1e-10,
+               q.int=-2, q.ind=-2,
+               h=1e-10)
+  upper.lims=c(b=1,
+               q.int=3, q.ind=3,
+               h=3)
+  bh.fixed.mod <- mle2(NLL.real.bh, start=start.vals, data=dd.ml,
+                       method="L-BFGS-B",
+                       lower=lower.lims,
+                       upper=upper.lims)
+  BH.Fixed.Mod<-list('Coefficients' = attributes(summary(bh.fixed.mod))$coef, 'AIC' = AIC(bh.fixed.mod))
+
+  print('B and H mode done!')
+
+  ### B and Q constant ###
+  start.vals = list(b=0.14,
+                    q=1.6,
+                    h.int=0.357, h.ind=1.68)
+  lower.lims=c(b=1e-10,
+               q=-2,
+               h.int=1e-10, h.ind=1e-10)
+  upper.lims=c(b=1,
+               q=3,
+               h.int=3, h.ind=3)
+  bq.fixed.mod <- mle2(NLL.real.bq, start=start.vals, data=dd.ml,
+                       method="L-BFGS-B",
+                       lower=lower.lims,
+                       upper=upper.lims)
+  BQ.Fixed.Mod<-list('Coefficients' = attributes(summary(bq.fixed.mod))$coef, 'AIC' = AIC(bq.fixed.mod))
+
+  print('B and Q mode done!')
+
+  ### H and Q constant ###
+  start.vals = list(b.int=0.14, b.ind=0.004,
+                    q=1.6,
+                    h=1.01)
+  lower.lims=c(b.int=1e-10, b.ind=1e-10,
+               q=-2,
+               h=1e-10)
+  upper.lims=c(b.int=1, b.ind=1,
+               q=3,
+               h=3)
+  hq.fixed.mod <- mle2(NLL.real.hq, start=start.vals, data=dd.ml,
+                       method="L-BFGS-B",
+                       lower=lower.lims,
+                       upper=upper.lims)
+  HQ.Fixed.Mod<-list('Coefficients' = attributes(summary(hq.fixed.mod))$coef, 'AIC' = AIC(hq.fixed.mod))
+
+  print('H and Q mode done!')
+
+  ### ALL Constant ###
+  start.vals = list(b=0.14,
+                    q=1.6,
+                    h=1.01)
+  lower.lims=c(b=1e-10,
+               q=-2,
+               h=1e-10)
+  upper.lims=c(b=1,
+               q=3,
+               h=3)
+  bqh.fixed.mod <- mle2(NLL.real.bhq, start=start.vals, data=dd.ml,
+                        method="L-BFGS-B",
+                        lower=lower.lims,
+                        upper=upper.lims)
+  BQH.Fixed.Mod<-list('Coefficients' = attributes(summary(bqh.fixed.mod))$coef, 'AIC' = AIC(bqh.fixed.mod))
+
+  results <- list('Full Model' = Full.Mod, 'B Fixed' = B.Fixed.Mod, 'H Fixed' = H.Fixed.Mod, 'Q Fixed' = Q.Fixed.Mod,
+                  'BH Fixed' = BH.Fixed.Mod, 'BQ Fixed' = BQ.Fixed.Mod, 'HQ Fixed' = HQ.Fixed.Mod, 'BQH Fixed', BQH.Fixed.Mod)
+
+  print('All done!')
+  return(results)
+
+}
+
+
+
+
+
 
 
--- R/InternalFunctions.R
+++ R/InternalFunctions.R
@@ -36,7 +36,7 @@ gradfun <- function(t,y,parms) {
 #that is, when prey are not being replaced throughout the experiment
 
 fun2 <- function(b,q,h,T, P,N0) {
-  require(deSolve)
+  library(deSolve)
   L <- lsoda(c(N=N0),
              times=c(0,T),
              func=gradfun,
@@ -46,13 +46,21 @@ fun2 <- function(b,q,h,T, P,N0) {
 }
 
 
+
+
+
+#### ONE TREATMENT ######
+
 ## Likelyhood function for one treatment scenario
 ## Initial=dd$offered Initial is a vector with offered inidividuals
 
-NLL.oneT = function(Initial, b, q, h, T, P) {
-  if (b < 0 || h < 0)
-    return(NA)
-  prop.exp = fun2(Initial, b, q, h, P, T)/Initial
+NLL.oneT = function(b, q, h, T = 4, P = 1) {
+  b <- b
+  q <- q
+  h <- h
+  prop.exp <- numeric(length=length(Initial))
+  prop.exp <- sapply(Initial, fun2,
+                     b=b, q=q, h=h, P=P, T=T)/Initial
   - sum(dbinom(Killed, prob = prop.exp, size = Initial,
                log = TRUE))
 }
@@ -61,6 +69,7 @@ NLL.oneT = function(Initial, b, q, h, T, P) {
 
 
 
+##### FULL MODEL TWO TREATMENTS ####
 
 ## Likelyhood function for two treatment scenario. Induced treatment == 0
 # ind is the induced vector, Initial is a vector with offered inidivuals
@@ -69,7 +78,7 @@ NLL.real.full <- function(b.int,b.ind,
                           q.int, q.ind,
                           h.int, h.ind,
                           T = 4, P = 1,
-                          ind, Initial) {
+                          ind) {
 
   ## NOTE ### here the parameters are not the same as before!!!
   ## IMPORTANT -- the parameters are the actual values, not the usual for R
@@ -87,6 +96,241 @@ NLL.real.full <- function(b.int,b.ind,
                              b=b.ind, q=q.ind, h=h.ind, P=P, T=T)/Initial[ind==1]
 
   print(sum(prop.exp))
+
+  - sum(dbinom(Killed, prob = prop.exp, size = Initial,
+               log = TRUE))
+}
+
+
+
+
+
+###### B IS CONSTANT BETWEEN TREATMENTS#######
+
+## Likelyhood function for two treatment scenario. Induced treatment == 0
+# ind is the induced vector, Initial is a vector with offered inidivuals
+# ### b is constant between treatments
+
+NLL.real.b <- function(b,
+                       q.int, q.ind,
+                       h.int, h.ind,
+                       T = 4, P = 1,
+                       ind) {
+
+  ## NOTE ### here the parameters are not the same as before!!!
+  ## IMPORTANT -- the parameters are the actual values, not the usual for R
+  ## This makes constraining the values easier
+  b <- b
+  q <- ifelse(ind==0, q.int, q.ind)
+  h <- ifelse(ind==0, h.int, h.ind)
+
+  print(c(b, h.int, h.ind, q.int, q.ind))
+
+  prop.exp <- numeric(length=length(Initial))
+  prop.exp[ind==0] <- sapply(Initial[ind==0], fun2,
+                             b=b, q=q.int, h=h.int, P=P, T=T)/Initial[ind==0]
+  prop.exp[ind==1] <- sapply(Initial[ind==1], fun2,
+                             b=b, q=q.ind, h=h.ind, P=P, T=T)/Initial[ind==1]
+
+  print(sum(prop.exp))
+
+  - sum(dbinom(Killed, prob = prop.exp, size = Initial,
+               log = TRUE))
+}
+
+
+
+
+
+###### H IS CONSTANT BETWEEN TREATMENTS#######
+
+## Likelyhood function for two treatment scenario. Induced treatment == 0
+# ind is the induced vector, Initial is a vector with offered inidivuals
+# ### h is constant between treatments
+
+NLL.real.h <- function(b.int,b.ind,
+                       q.int, q.ind,
+                       h,
+                       T = 4, P = 1,
+                       ind) {
+
+  ## NOTE ### here the parameters are not the same as before!!!
+  ## IMPORTANT -- the parameters are the actual values, not the usual for R
+  ## This makes constraining the values easier
+  b <- ifelse(ind==0, b.int, b.ind)
+  q <- ifelse(ind==0, q.int, q.ind)
+  h <- h
+
+  prop.exp <- numeric(length=length(Initial))
+  prop.exp[ind==0] <- sapply(Initial[ind==0], fun2,
+                             b=b.int, q=q.int, h=h, P=P, T=T)/Initial[ind==0]
+  prop.exp[ind==1] <- sapply(Initial[ind==1], fun2,
+                             b=b.ind, q=q.ind, h=h, P=P, T=T)/Initial[ind==1]
+
+
+  - sum(dbinom(Killed, prob = prop.exp, size = Initial,
+               log = TRUE))
+}
+
+
+
+
+
+###### Q IS CONSTANT BETWEEN TREATMENTS#######
+
+## Likelyhood function for two treatment scenario. Induced treatment == 0
+# ind is the induced vector, Initial is a vector with offered inidivuals
+# ### q is constant between treatments
+
+NLL.real.q <- function(b.int,b.ind,
+                       q,
+                       h.int, h.ind,
+                       T = 4, P = 1,
+                       ind) {
+
+  ## NOTE ### here the parameters are not the same as before!!!
+  ## IMPORTANT -- the parameters are the actual values, not the usual for R
+  ## This makes constraining the values easier
+  b <- ifelse(ind==0, b.int, b.ind)
+  q <- q
+  h <- ifelse(ind==0, h.int, h.ind)
+
+  prop.exp <- numeric(length=length(Initial))
+  prop.exp[ind==0] <- sapply(Initial[ind==0], fun2,
+                             b=b.int, q=q, h=h.int, P=P, T=T)/Initial[ind==0]
+  prop.exp[ind==1] <- sapply(Initial[ind==1], fun2,
+                             b=b.ind, q=q, h=h.ind, P=P, T=T)/Initial[ind==1]
+
+
+  - sum(dbinom(Killed, prob = prop.exp, size = Initial,
+               log = TRUE))
+}
+
+
+
+
+
+###### B and H IS CONSTANT BETWEEN TREATMENTS#######
+
+## Likelyhood function for two treatment scenario. Induced treatment == 0
+# ind is the induced vector, Initial is a vector with offered inidivuals
+# ### b and h is constant between treatments
+
+
+NLL.real.bh <- function(b,
+                        q.int, q.ind,
+                        h,
+                        T = 4, P = 1,
+                        ind) {
+
+  ## NOTE ### here the parameters are not the same as before!!!
+  ## IMPORTANT -- the parameters are the actual values, not the usual for R
+  ## This makes constraining the values easier
+  b <- b
+  q <- ifelse(ind==0, q.int, q.ind)
+  h <- h
+
+  prop.exp <- numeric(length=length(Initial))
+  prop.exp[ind==0] <- sapply(Initial[ind==0], fun2,
+                             b=b, q=q.int, h=h, P=P, T=T)/Initial[ind==0]
+  prop.exp[ind==1] <- sapply(Initial[ind==1], fun2,
+                             b=b, q=q.ind, h=h, P=P, T=T)/Initial[ind==1]
+
+
+  - sum(dbinom(Killed, prob = prop.exp, size = Initial,
+               log = TRUE))
+}
+
+
+
+###### B and Q IS CONSTANT BETWEEN TREATMENTS#######
+
+## Likelyhood function for two treatment scenario. Induced treatment == 0
+# ind is the induced vector, Initial is a vector with offered inidivuals
+# ### b and q is constant between treatments
+
+NLL.real.bq <- function(b,
+                        q,
+                        h.int, h.ind,
+                        T = 4, P = 1,
+                        ind) {
+
+  ## NOTE ### here the parameters are not the same as before!!!
+  ## IMPORTANT -- the parameters are the actual values, not the usual for R
+  ## This makes constraining the values easier
+  b <- b
+  q <- q
+  h <- ifelse(ind==0, h.int, h.ind)
+
+  prop.exp <- numeric(length=length(Initial))
+  prop.exp[ind==0] <- sapply(Initial[ind==0], fun2,
+                             b=b, q=q, h=h.int, P=P, T=T)/Initial[ind==0]
+  prop.exp[ind==1] <- sapply(Initial[ind==1], fun2,
+                             b=b, q=q, h=h.ind, P=P, T=T)/Initial[ind==1]
+
+
+  - sum(dbinom(Killed, prob = prop.exp, size = Initial,
+               log = TRUE))
+}
+
+
+###### H and Q IS CONSTANT BETWEEN TREATMENTS#######
+
+## Likelyhood function for two treatment scenario. Induced treatment == 0
+# ind is the induced vector, Initial is a vector with offered inidivuals
+# ### H and q is constant between treatments
+
+
+NLL.real.hq <- function(b.int,b.ind,
+                        q,
+                        h,
+                        T = 4, P = 1,
+                        ind) {
+
+  ## NOTE ### here the parameters are not the same as before!!!
+  ## IMPORTANT -- the parameters are the actual values, not the usual for R
+  ## This makes constraining the values easier
+  b <- ifelse(ind==0, b.int, b.ind)
+  q <- q
+  h <- h
+
+  prop.exp <- numeric(length=length(Initial))
+  prop.exp[ind==0] <- sapply(Initial[ind==0], fun2,
+                             b=b.int, q=q, h=h, P=P, T=T)/Initial[ind==0]
+  prop.exp[ind==1] <- sapply(Initial[ind==1], fun2,
+                             b=b.ind, q=q, h=h, P=P, T=T)/Initial[ind==1]
+
+
+  - sum(dbinom(Killed, prob = prop.exp, size = Initial,
+               log = TRUE))
+}
+
+
+
+### ALL CONTANT###
+## Likelyhood function for two treatment scenario. Induced treatment == 0
+# ind is the induced vector, Initial is a vector with offered inidivuals
+# ### ALL constant between treatments
+
+NLL.real.bhq <- function(b,
+                         q,
+                         h,
+                         T = 4, P = 1,
+                         ind) {
+
+  ## NOTE ### here the parameters are not the same as before!!!
+  ## IMPORTANT -- the parameters are the actual values, not the usual for R
+  ## This makes constraining the values easier
+  b <- b
+  q <- q
+  h <- h
+
+  prop.exp <- numeric(length=length(Initial))
+  prop.exp[ind==0] <- sapply(Initial[ind==0], fun2,
+                             b=b, q=q, h=h, P=P, T=T)/Initial[ind==0]
+  prop.exp[ind==1] <- sapply(Initial[ind==1], fun2,
+                             b=b, q=q, h=h, P=P, T=T)/Initial[ind==1]
+
 
   - sum(dbinom(Killed, prob = prop.exp, size = Initial,
                log = TRUE))
